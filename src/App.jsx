cb3ed2e819cd8e1b0349fe1baddbebafd551917f
Maciej <msypniewski511@gmail.com>
2024-10-30 21:06:16 +0000

    feat: add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Blog } from './Blog.jsx'
 
 const queryClient = new QueryClient()
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Blog />,
+  },
+])
+
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Blog />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 }
